3414df96d9afaaceb31d3ec73b9047c7b280fc93
chopbk <dinhtam94@gmail.com>
2019-06-04 22:47:19 +0700

    Write a shell: - Chang function shell_read_line - add some file to gitignored

--- LearnC/write_a_shell/shell.c
+++ LearnC/write_a_shell/shell.c
@@ -44,40 +44,15 @@ void shell_loop(void)
  */
 char * shell_read_line(void)
 {
-  int bufsize = SHELL_RL_BUFSIZE;
-  int position = 0;
-  char *buffer = malooc(sizeof(char) * bufsize);
-  int c;
-
-  if(!buffer)
-  {
-    fprintf(stderr, "shell: allocation error\n");
-    exit(EXIT_FAILURE);
-  }
-  while(1) {
-    //read a character
-    c = getchar();
-
-    // If we hit EOF, replace it with a null character and return
-    if(c == EOF || c == '\n') {
-      buffer[position] = '\0';
-      return buffer;
-    } else {
-      buffer[position] = c;
-    }
-    position++;
-
-    //If we have exeeded the buffer, reallocate.
-    if(position >= bufsize){
-      bufsize += SHELL_RL_BUFSIZE;
-      buffer = realloc(buffer, bufsize);
-        if(!buffer)
-        {
-          fprintf(stderr, "shell: allocation error\n");
-          exit(EXIT_FAILURE);
-        }
-    }
-  }
+  char *line = NULL;
+  ssize_t bufsize = 0; // have getline allocate a buffer for use
+  /**
+   * Function getline() reads an entire line from stream, storing the address of
+   * the buffer containing the text into *lineptr.  The buffer is null-
+   * terminated and includes the newline character, if one was found
+  */
+  getline(&line, &bufsize, stdin);
+  return line;
 }
 
 /**
